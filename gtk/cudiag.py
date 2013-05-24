536dd80f790386e9488631bdfb249ec96dbc38fc
Tomasz Wyderka <wyderkat@cofoh.com>
2013-05-25 00:39:33 +0700

    a

--- gtk/cudiag.py
+++ gtk/cudiag.py
@@ -29,10 +29,53 @@ class CUDiag(Gtk.ScrolledWindow):
     cr.select_font_face("tahoma")
   
     cr.set_source_rgb(0.0, 0.0, 0.0)
-    me.draw_tree ( cr, me.root, 0, 2*pi, 0.5, 0.5 )
+    me.draw_tree ( cr, me.root )
     return False
          
-  def draw_tree( me , cr, fun, deg, dlen, x, y, scale=0.0 ):
+  def draw_tree( me , cr, fun, 
+                deg=0.0, dlen=2.0*pi, 
+                xcen=0.5, ycen=0.5, arm=0.0, 
+                xpre=0.5, ypre=0.5,
+                scale=0.0 ):
+    elipsis_ratio = 2.5
+    e = cr.text_extents( fun.name ) 
+    x = xcen + elipsis_ratio*sin(deg + dlen/2)*arm
+    y = ycen - cos(deg + dlen/2)*arm
+    cr.move_to( xpre, ypre )
+    cr.line_to( x, y ) 
+    cr.stroke()
+    cr.move_to(x - e[2]/2 - e[0], 
+               y - e[3]/2 - e[1]) 
+    cr.text_path( fun.name )
+    cr.fill()
+    if scale == 0.0: #not set, first function
+      scale = e[3]*1.8 # TODO tmp solution
+    r = ( e[2]+e[3]*0.8, scale )
+    me.round_rectangle( cr, x - r[0]/2, y - r[1]/2, r[0], r[1], 1.0, r[1]/3)
+    print  x - r[0]/2, y - r[1]/2, r[0], r[1], 1.0, r[1]/3
+    #
+    childs = fun.what_strong_layer_childs_fun()
+    if childs:
+      n_childs = len(childs)
+      sub_dlen = dlen / n_childs
+      # wzor na ramie
+      # dlen*new_arm (OBWOD) = n_childs*min_size
+      min_size = 2*scale
+      new_arm = (n_childs * min_size) / dlen
+      if n_childs == 1:
+        new_arm = arm + min_size
+
+      i_deg = deg
+      for c in childs:
+        print "%s - new_arm=%g for hight=%g and childsno=%d" %\
+        (c.name, new_arm, r[1], n_childs)
+        x1 = x+elipsis_ratio*sin(i_deg + sub_dlen/2)*arm
+        y1 = y-cos(i_deg + sub_dlen/2)*arm
+        #print "%s at x1=%g, y1=%g, deg=%g " % (c.name, x1, y1, i_deg)
+        me.draw_tree( cr, c, i_deg, sub_dlen, xcen, ycen, new_arm, x, y, scale )
+        i_deg += sub_dlen
+
+  def draw_tree_OLD( me , cr, fun, deg, dlen, x, y, scale=0.0 ):
     elipsis_ratio = 2.5
     e = cr.text_extents( fun.name ) 
     cr.move_to(x - e[2]/2 - e[0], y - e[3]/2 - e[1])
